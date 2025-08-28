278f0e34987642b4f4e1d9d1e44b948899531fa8
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-29 01:06:19 +0200

    feat(validations): create layer-independent alias for a form field

---
+++ app/src/main/java/com/stridehabits/app/core/models/FormField.kt
@@ -0,0 +1,30 @@
+package com.stridehabits.app.core.models
+
+import com.stridehabits.app.core.validations.ValidationError
+import com.stridehabits.app.core.validations.statutes.ValidationLaw
+
+/**
+ * Domain-layer abstraction for a form field with validation rules.
+ *
+ * @param value The value of the field; should have an initial state set.
+ * @param rules The order in which you provide the [ValidationLaw] collection is
+ *              important as this is the order in which any violations will send
+ *              back to the receiver. Ensure that a logical order is followed.
+ * @param error The error state of the field; should be mapped to the UI.
+ */
+data class FormField<T>(
+    val value: T,
+    val rules: List<ValidationLaw<T>> = emptyList(),
+    val error: ValidationError? = null
+) {
+    val isValid: Boolean get() = (error == null)
+
+
+    fun applyValidation(): FormField<T> {
+        val firstValidationError = rules.firstNotNullOfOrNull { validationLaw ->
+            validationLaw.checkFor(value)
+        }
+
+        return this.copy(error = firstValidationError)
+    }
+}
---
+++ app/src/main/java/com/stridehabits/app/ui/shared/models/ValidatableForm.kt
@@ -0,0 +1,33 @@
+package com.stridehabits.app.ui.shared.models
+
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Used in composition patterns for a ViewModel servicing multiple forms.
+ *
+ * **Example:**
+ *
+ * ```
+ * val signUpForm = SignUpFormViewModel()
+ * val signInForm = SignInFormViewModel()
+ * private val formList: List<ValidatableForm> = listOf(
+ *     signUpForm,
+ *     signInForm,
+ * )
+ *
+ *
+ * val isAllFormsValid = combine(
+ *     formList.map { it.isFormValid }
+ * ) {
+ *     it.all { isValid ->
+ *         isValid
+ *     }
+}.stateIn(
+ *     viewModelScope, SharingStarted.Eagerly, false
+ * )
+ * ```
+ */
+interface ValidatableForm {
+    val isFormValid: StateFlow<Boolean>
+    fun validateForm()
+}
